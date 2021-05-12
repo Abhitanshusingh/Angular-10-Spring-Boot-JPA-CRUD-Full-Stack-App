181e73c498516d05fa168f9971f2f7cc3e5d7d7e
abhitanshu singh <abhitanshusingh@gmail.com>
2021-05-12 23:27:17 +0530

    ADDED: Get Employee to show emlployee on screen

---
+++ FrontEndEmployeeManagementSystem/src/app/employee.ts
@@ -0,0 +1,9 @@
+// This class is used to give back response
+export class Employee {
+    id: number
+    firstName:String
+    lastName:String
+    address:String
+    mobileNumber:String
+    city:String
+}
