1663c702ed09dc641267420b6e8b17ea9b952545
abhitanshu singh <abhitanshusingh@gmail.com>
2021-05-17 13:46:44 +0530

    ADDED: Create Employee method to add employee

--- BackEndEmployeeManagementSystem/src/main/java/com/baghel/controller/EmployeeController.java
+++ BackEndEmployeeManagementSystem/src/main/java/com/baghel/controller/EmployeeController.java
@@ -3,25 +3,34 @@ package com.baghel.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.baghel.model.Employee;
-import com.baghel.repository.EmployeeRepository;
+import com.baghel.service.EmployeeService;
 
 @RestController
 @RequestMapping("/api/baghelv1/")
 public class EmployeeController {
 
 	@Autowired
-	EmployeeRepository employeeRepository;
+	EmployeeService employeeService;
 
-	@CrossOrigin(origins = "http://localhost:4200")
+	@CrossOrigin
 	@GetMapping("/employees")
-	private List<Employee> getAllEmployee() {
-		return employeeRepository.findAll();
+	private ResponseEntity<List<Employee>> getAllEmployee() {
+		return ResponseEntity.ok().body(employeeService.getAllEmployee());
+	}
+	
+	@CrossOrigin
+	@PostMapping("/employees")
+	private Employee createEmployee(@RequestBody Employee employee){
+		return employeeService.addEmployee(employee);
 	}
 
 }
