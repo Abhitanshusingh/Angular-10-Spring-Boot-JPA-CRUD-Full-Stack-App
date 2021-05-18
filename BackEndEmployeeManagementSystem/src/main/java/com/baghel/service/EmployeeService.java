4066ff8c15a4faf7b71301ddf6fa557874891a5b
abhitanshu singh <abhitanshusingh@gmail.com>
2021-05-18 20:05:13 +0530

    ADDED: Get method and Update method to serve Employee

--- BackEndEmployeeManagementSystem/src/main/java/com/baghel/service/EmployeeService.java
+++ BackEndEmployeeManagementSystem/src/main/java/com/baghel/service/EmployeeService.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.baghel.exception.ResourceNotFoundException;
 import com.baghel.model.Employee;
 import com.baghel.repository.EmployeeRepository;
+import com.sun.el.stream.Optional;
 
 @Service
 public class EmployeeService {
@@ -21,5 +23,24 @@ public class EmployeeService {
 	public Employee addEmployee(Employee employee) {
 		return employeeRepository.save(employee);
 	}
+
+	public Employee getEmployeeById(Long id) {
+		Employee employeeObj = employeeRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee not exist by id:" +id));
+		return employeeObj;
+	}
+
+	public Employee updateEmployee(Long id, Employee employee) {
+		Employee employeeObj = employeeRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee not exist by id:" +id));
+		employee.setFirstName(employee.getFirstName());
+		employee.setLastName(employee.getLastName());
+		employee.setCity(employee.getCity());
+		employee.setMobileNumber(employee.getMobileNumber());
+		employee.setAddress(employee.getAddress());
+		Employee updatedEmployee = employeeRepository.save(employee);
+		return updatedEmployee;
+	}
+	
 	
 }
