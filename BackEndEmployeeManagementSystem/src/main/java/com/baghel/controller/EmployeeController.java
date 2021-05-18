3b7d7e1aee854f33eefceaffa0192266ac262034
abhitanshu singh <abhitanshusingh@gmail.com>
2021-05-18 19:54:43 +0530

    ADDED: GetEmployeeById and Update Employee API

--- BackEndEmployeeManagementSystem/src/main/java/com/baghel/controller/EmployeeController.java
+++ BackEndEmployeeManagementSystem/src/main/java/com/baghel/controller/EmployeeController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,8 +31,21 @@ public class EmployeeController {
 	
 	@CrossOrigin
 	@PostMapping("/employees")
-	private Employee createEmployee(@RequestBody Employee employee){
-		return employeeService.addEmployee(employee);
+	private ResponseEntity<Employee> createEmployee(@RequestBody Employee employee){
+		return ResponseEntity.ok().body(employeeService.addEmployee(employee));
 	}
-
+	
+	@CrossOrigin
+	@GetMapping("/employees/{id}")
+	private ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+		return ResponseEntity.ok().body(employeeService.getEmployeeById(id));
+	}
+	
+	@CrossOrigin
+	@PutMapping("/employees/{id}")
+	private ResponseEntity<Employee> updateEmployee(@PathVariable Long id,@RequestBody Employee employee) {
+		return ResponseEntity.ok().body(employeeService.updateEmployee(id,employee));
+		
+	}
+		
 }
