1ccf0cc551083e196ab75a58e3f371ea777e9bb3
abhitanshu singh <abhitanshusingh@gmail.com>
2021-05-20 15:53:35 +0530

    ADDED: Delete Api

--- BackEndEmployeeManagementSystem/src/main/java/com/baghel/controller/EmployeeController.java
+++ BackEndEmployeeManagementSystem/src/main/java/com/baghel/controller/EmployeeController.java
@@ -1,10 +1,12 @@
 package com.baghel.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,10 +14,10 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.baghel.model.Employee;
 import com.baghel.service.EmployeeService;
 
+@CrossOrigin(origins="*")
 @RestController
 @RequestMapping("/api/baghelv1/")
 public class EmployeeController {
@@ -23,29 +25,31 @@ public class EmployeeController {
 	@Autowired
 	EmployeeService employeeService;
 
-	@CrossOrigin
+	
 	@GetMapping("/employees")
 	private ResponseEntity<List<Employee>> getAllEmployee() {
 		return ResponseEntity.ok().body(employeeService.getAllEmployee());
 	}
 	
-	@CrossOrigin
 	@PostMapping("/employees")
 	private ResponseEntity<Employee> createEmployee(@RequestBody Employee employee){
 		return ResponseEntity.ok().body(employeeService.addEmployee(employee));
 	}
 	
-	@CrossOrigin
 	@GetMapping("/employees/{id}")
 	private ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
 		return ResponseEntity.ok().body(employeeService.getEmployeeById(id));
 	}
 	
-	@CrossOrigin
 	@PutMapping("/employees/{id}")
 	private ResponseEntity<Employee> updateEmployee(@PathVariable Long id,@RequestBody Employee employee) {
 		return ResponseEntity.ok().body(employeeService.updateEmployee(id,employee));
 		
+	}
+	
+	@DeleteMapping("employee/{id}")
+	private ResponseEntity<Map<String,Boolean>> deleteEmployee(@PathVariable Long id,@RequestBody Employee employee){
+		return ResponseEntity.ok().body(employeeService.deleteEmployee(id,employee));
 	}
 		
 }
