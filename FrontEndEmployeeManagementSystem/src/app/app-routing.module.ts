7501725b5b08d637de79a81e32df6331d8d04f19
abhitanshu singh <abhitanshusingh@gmail.com>
2021-05-20 16:00:25 +0530

    ADDED: Delete Employee router URL

--- FrontEndEmployeeManagementSystem/src/app/app-routing.module.ts
+++ FrontEndEmployeeManagementSystem/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { CreateEmployeeComponent} from './create-employee/create-employee.component'
 import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
+import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
 
 const routes: Routes = [ 
   {path:'employee', component:EmployeeListComponent},
   {path:'', redirectTo: 'employee', pathMatch: 'full'},
   {path:'create-employee', component:CreateEmployeeComponent},
-  {path:'update-employee/:id', component:UpdateEmployeeComponent}
+  {path:'update-employee/:id', component:UpdateEmployeeComponent},
+  {path:'employee-detail/:id', component:EmployeeDetailComponent}
 ];
 
 @NgModule({
